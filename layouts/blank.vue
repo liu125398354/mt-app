3979a49ec24f16206b128294009ace82b60ef590
liu125398354 <15111390477@163.com>
2019-09-25 19:31:33 +0800

    new add register.vue && add some server register verification

---
+++ layouts/blank.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout-blank">
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ server/interface/users.js
@@ -0,0 +1,46 @@
+import Router from 'koa-router'
+import Redis from 'koa-redis'
+import nodeMailer from 'nodemailer'
+
+import User from '../dbs/models/user'
+import Passport from './utils/passport'
+import Email from '../dbs/config'
+import axios from './utils/axios'
+
+let router = new Router({
+  prefix: '/users'
+})
+
+let Store = new Redis().client
+
+router.post('/signup', async(ctx) => {
+  const {
+    username,
+    password,
+    email,
+    code
+  } = ctx.request.body
+  if (code) {
+    const saveCode = await Store.hget(`nodemail: ${username}`, 'code')
+    const saveExpire = await Store.hget(`nodemail: ${username}`, 'expire')
+    if (code === saveCode) {
+      if (new Date().getTime() - saveExpire > 0) {
+        ctx.body = {
+          code: -1,
+          msg: '验证码已过期，请重新尝试'
+        }
+        return false
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: '请填写正确的验证码'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '请填写验证码'
+    }
+  }
+})
